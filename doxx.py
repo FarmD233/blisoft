cdbe50f1e7e82adf0d3da02559b0f42051556593
FarmD233 <136690657+FarmD233@users.noreply.github.com>
2024-09-27 20:22:54 +0700

    Add files via upload

---
+++ doxx.py
@@ -0,0 +1,16 @@
+import socket
+import requests
+import pprint
+
+data = input('Ketik 1 untuk melakukan doxx: ')
+
+print(f'Nama : Randika Norhaizal')
+print(f'TTL : TanjungPinang 07-10-1995')
+print(f'Agama : Islam')
+print(f'Jenis Kelamin : Laki - Laki')
+print(f'NIK : 2172040710950004')
+print(f'Status : Kepala Keluarga')
+print(f'Gol Darah : -')
+print(f'Alamat : JL. MAPUR NO. 13, Prov. Riau, Kec. Bukit Bestari, Desa Seijang')
+print(f'Kewarganegaraan : WNI')
+print(f'Portal Zip : 29124')
